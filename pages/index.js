894370425a86feea32eca32496f4d3842539b5f9
jay dave <20bmiit071@gmail.com>
2024-08-18 19:26:58 +0530

    4th try

--- pages/index.js
+++ pages/index.js
@@ -140,7 +140,7 @@ export default function Home() {
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
-              for both business and consumer use.
+              for both business &apos; consumer use.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               I offer from a wide range of services, including brand design,
